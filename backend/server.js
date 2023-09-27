99f9fa93d387e5ecdcb44c6d3df9587471aa2406
Victor Leman <victorleman1@gmail.com>
2023-09-27 18:28:22 +0300

    updated

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,6 @@ const connectDB = require("./config/db");
 const dotenv = require("dotenv").config();
 const port = 5010;
 const cors = require("cors");
-/* const multer = require("multer");
-const path = require("path"); */
 
 //connexion à la db
 connectDB();
@@ -12,12 +10,12 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://moulincasta.onrender.com",
+    origin: "http://moulincasta.onrender.com" /*  "http://localhost:3000" */,
     credentials: true,
     optionsSuccessStatus: 200,
   })
 );
-//Middleware qui permet de traiter les données de la requestapp.use(express.static("public"));
+//Middleware qui permet de traiter les données de la request
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ error: "Internal Server Error" });
@@ -27,28 +25,6 @@ app.use(express.urlencoded({ extended: false }));
 //
 app.use("/defaultRate", require("./routes/defaultRate.routes"));
 app.use("/rate", require("./routes/rate.routes"));
-// app.use("/img", require("./routes/img.routes"));
-//Upload Images
-
-/* const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "public/images");
-  },
-  filename: (req, file, cb) => {
-    cb(
-      null,
-      file.fieldname + "_" + Date.now() + path.extname(file.originalname)
-    );
-  },
-});
-const upload = multer({
-  storage: storage,
-});
-app.post("/upload", upload.single("images"), (req, res) => {
-  ImgModel.create({ img: req.file.filename })
-    .then((result) => res.json(result))
-    .catch((err) => console.log(err));
-}); */
 
 // lancer le serveur
 app.listen(port, () => console.log("le serveur a démarré au port " + port));
