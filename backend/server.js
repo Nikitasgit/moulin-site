db05175c3420ab693a0f1d79814fa7bd2af975d3
Victor Leman <victorleman1@gmail.com>
2023-09-27 15:39:58 +0300

    commit

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://moulincasta.onrender.com",
     credentials: true,
     optionsSuccessStatus: 200,
   })
