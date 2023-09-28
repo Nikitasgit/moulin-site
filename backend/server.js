8124ca9e7849d2ed663f905b5f9a106f00c40a6f
Victor Leman <victorleman1@gmail.com>
2023-09-28 18:56:09 +0300

    commit

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://moulincasta.netlify.app",
     credentials: true,
     optionsSuccessStatus: 200,
   })
